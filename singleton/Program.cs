410df7ee52a105adabcd782fdc8cc5d4ed2cf628
mahin725 <rihanthedev@gmail.com>
2026-05-19 02:19:07 +0600

    singleton

---
+++ singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Singleton
+{
+    private static Singleton _instance;
+    private Singleton()
+    {
+    }
+    public static Singleton GetInstance()
+    {
+        if (_instance == null)
+        { _instance = new Singleton(); }
+        return _instance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Singleton obj1 = Singleton.GetInstance();
+        Singleton obj2 = Singleton.GetInstance();
+        // now check two object are same or not... definitely will same. 
+        Console.WriteLine(obj1==obj2);
+    }
+}
