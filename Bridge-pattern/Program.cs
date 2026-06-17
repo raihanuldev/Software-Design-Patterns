f1d2982d7f70afe28b908f3ae82d199de063550b
mahin725 <rihanthedev@gmail.com>
2026-06-18 04:37:22 +0600

    Add generated files and update assembly information across multiple projects
    - Added generated MSBuild editor config and global usings for Bridge-pattern project.
    - Created asset cache, compile inputs cache, and file list for Bridge-pattern.
    - Generated binaries and source link JSON for Bridge-pattern.
    - Updated assembly information for Builder, Decorator, Factory, Prototype, Proxy, and Singleton projects with new informational version.
    - Updated project assets and cache files for Proxy-pattern and Singleton projects.

---
+++ Bridge-pattern/Program.cs
@@ -0,0 +1,63 @@
+﻿public interface IPaymentGateway
+{
+    public void ProccessPayment(double amount);
+}
+
+// StripeGateWay
+public class StripeGateWay : IPaymentGateway
+{
+    public void ProccessPayment(double amount)
+    {
+        System.Console.WriteLine($"Proccess Payment {amount} by Stripe");
+    }
+}
+//UPIPayment
+public class UPIPaymentGateWay : IPaymentGateway
+{
+    public void ProccessPayment(double amount)
+    {
+        System.Console.WriteLine($"Proccess Payment {amount} by UPIPaymentGateWay");
+    }
+}
+
+public abstract class PaymentMethod
+{
+    public IPaymentGateway _gateway;
+    public PaymentMethod(IPaymentGateway gateway)
+    {
+        _gateway = gateway;
+    }
+    public abstract void Pay(double amount);
+}
+
+public class CardPayment : PaymentMethod
+{
+    public CardPayment(IPaymentGateway _gateway):base(_gateway){}
+    public override void Pay(double amount)
+    {
+        System.Console.WriteLine("card Payment.....");
+        _gateway.ProccessPayment(amount);
+        
+    }
+}
+public class UPIPayment : PaymentMethod
+{
+    public UPIPayment(IPaymentGateway _gateway):base(_gateway){}
+    public override void Pay(double amount)
+    {
+        System.Console.WriteLine("UPIPayment Payment.....");
+        _gateway.ProccessPayment(amount);
+        
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        PaymentMethod p1 = new CardPayment(new StripeGateWay());
+        p1.Pay(500);
+        PaymentMethod p2 = new UPIPayment(new UPIPaymentGateWay());
+        p2.Pay(1000
+        );
+    }
+}
--- singleton/obj/Debug/net10.0/singleton.AssemblyInfo.cs
+++ singleton/obj/Debug/net10.0/singleton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bf139a9429825355f6a8da5b14d4b3598c9cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8028374f075fec087b9aea7af0538c915995ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
